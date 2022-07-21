49c136bac2a49a57a24a5ade096e715d45401108
Vitalii Drobit <cdrobitvitalii@gmail.com>
2022-07-21 23:00:10 +0300

    blabla

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:3000', credentials: true}))
+app.use(cors({origin: 'https://vitaliidc.github.io', credentials: true}))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
